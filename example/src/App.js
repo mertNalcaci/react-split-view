a49533271602e0793c56639b1a87454efb06b456
Mert Nalcaci <mert@jotform.com>
2021-01-13 01:34:08 +0300

    Example updated with switcherProps

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { SplitView } from 'react-split-viewport';
 
 const App = () => {
+  const [isMinimized, setIsMinimized] = useState(false);
 
   return (
   	<SplitView
+      initWidth={40}
       range={{
-        min: 0,
+        min: 20,
         max: 100
+      }}
+      onResize={({ isMinimized }) => setIsMinimized(isMinimized)}
+      switcherProps={{
+        onSwitchClick: ({ isMinimized }) => setIsMinimized(isMinimized),
+        markup: <button>{isMinimized ? 'Maximize' : 'Minimize'}</button>
       }}
   	/>
   );
-}
+};
 
 export default App;
